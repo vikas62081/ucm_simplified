f558a0e0aa677be32e3a251b2c20c06de0bedd57
Aravind Jitta <rvindjitta11@gmail.com>
2024-05-13 17:01:33 -0500

    Added loop in my AppRouter for loopin router paths

--- src/routing/AppRouter.tsx
+++ src/routing/AppRouter.tsx
@@ -3,14 +3,25 @@ import Layout from "../components/layout";
 import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/AboutPage";
 
+const AppPaths = [
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+];
+
 const AppRouter = () => {
   return (
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          {/* <Route path="*" element={<NotFoundPage />} /> */}
+          {AppPaths.map((item) => (
+            <Route path={item.path} element={item.element} />
+          ))}
         </Routes>
       </Layout>
     </Router>
