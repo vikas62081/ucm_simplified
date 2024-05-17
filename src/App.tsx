ad350a4dfd9855661b72f2c992f0d6fc2a4e209b
Mohan-Avinash <mohan.avinash.k@gmail.com>
2024-05-16 21:09:26 -0500

    created subject card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import "./App.css";
 import AppRouter from "./routing/AppRouter";
+// import AccommodationDetails from "./AccomodationDetails";
 
 const App = () => {
   return <AppRouter />;
 };
+
+
+// const Counter = () => {
+//   // const { } = props;
+//   return (
+//     <div className="text-xs bg-gray-700 text-white p-3 rounded-lg box-border}">
+//       <div className="text-xl">Counter</div>
+//       <div className="items-center bg-red-500 inline-flex rounded-3xl">
+//       <button className="text-base text-white p-4 rounded-2xl">-</button>
+//       <button className="text-base text-white p-4 rounded-2xl">Reset</button>
+//       <button className="text-base text-white p-4 rounded-2xl">+</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+{/* <AccommodationDetails/> */}
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}", // Make sure this matches the file types you are using
   ],
   theme: {
-    extend: {     
+    extend: {   
+      boxShadow: {
+        'custom': '0 4px 4px rgba(0, 0, 0, 0.25)',
+      },  
       colors: {
         'custom-dark': '#242424', // Custom color
           primary: '#00796B',   // Replace '#4f46e5' with your primary color
